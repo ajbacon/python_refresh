05e8dd382b111ac0f6733aa224fe3a6e6444a636
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-21 18:27:25 +0100

    feat: add Deck class

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -9,5 +9,13 @@ class Card:
         return f"{self.value} of {self.suit}"
 
 
+class Deck:
+    values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+    def __init__(self):
+        pass
+
+
 card = Card("A", "Clubs")
 print(card)
