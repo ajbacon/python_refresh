4d7fcd8db357c7d7ca6d4b6f84a0ac6304fa0788
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-21 21:41:11 +0100

    feat: add deal cards and shuffle methods

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,3 +1,4 @@
+from random import shuffle
 
 
 class Card:
@@ -12,6 +13,8 @@ class Card:
 class Deck:
     def __init__(self):
         self.cards = self._create_deck()
+
+    # def __repr__(self):
 
     def _create_deck(self):
         values = ["A", "2", "3", "4", "5", "6",
@@ -30,13 +33,24 @@ class Deck:
             raise ValueError('All cards have been dealt')
         dealt = self.cards[-to_deal:]
         del self.cards[-to_deal:]
-        # self.cards = self.cards[:-to_deal]
-        print(self.cards)
         return dealt
+
+    def deal_card(self):
+        return self._deal(1)[0]
+
+    def deal_hand(self, num):
+        return self._deal(num)
+
+    def shuffle(self):
+        if self.count < 52:
+            raise ValueError('Only full decks can be shuffled')
+
+        shuffle(self.cards)
+        return self
 
 
 card = Card("A", "Clubs")
 print(card)
 deck = Deck()
 print(deck.count())
-deck._deal(1)
+print(deck.deal_card)
