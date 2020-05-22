049f4ddc8885002269a5a8e184a8daaf9f7f6be8
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-22 20:57:02 +0100

    refactor: use setUp to refactor tests

--- test/test_deck_of_cards.py
+++ test/test_deck_of_cards.py
@@ -7,28 +7,27 @@ from modules.deck_of_cards import Deck, Card
 
 
 class DeckOfCardsTests(unittest.TestCase):
+    def setUp(self):
+        self.deck = Deck()
+
     def test_countt(self):
-        deck = Deck()
-        self.assertEqual(deck.count(), 52)
+        self.assertEqual(self.deck.count(), 52)
 
     def test_deal_card(self):
-        deck = Deck()
-        dealt = deck.deal_card()
+        dealt = self.deck.deal_card()
         self.assertIsInstance(dealt, Card)
-        self.assertEqual(deck.count(), 51)
+        self.assertEqual(self.deck.count(), 51)
 
     def test_deal_hand_success(self):
-        deck = Deck()
-        dealt = deck.deal_hand(2)
+        dealt = self.deck.deal_hand(2)
         self.assertEqual(len(dealt), 2)
-        self.assertEqual(deck.count(), 50)
+        self.assertEqual(self.deck.count(), 50)
         self.assertIsInstance(dealt[0], Card)
         self.assertIsInstance(dealt[1], Card)
 
     def test_deal_hand_raise(self):
-        deck = Deck()
-        deck.deal_hand(52)
-        self.assertRaises(ValueError, deck.deal_hand, 1)
+        self.deck.deal_hand(52)
+        self.assertRaises(ValueError, self.deck.deal_hand, 1)
 
 
 if __name__ == "__main__":
